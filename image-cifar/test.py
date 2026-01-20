78a1eec347013dad00bd12c04e49bd83ba3788fc
Zesen <zesen@kth.se>
2026-01-20 14:06:27 +0100

    feat: figuring out right launch method

--- image-cifar/test.py
+++ image-cifar/test.py
@@ -1,7 +1,4 @@
 import os
-
-os.environ["PJRT_DEVICE"] = "TPU"
-
 import torch
 import torch.distributed as dist
 import torch_xla
@@ -12,7 +9,7 @@ def run(pass_rank: int):
     rank = xr.global_ordinal()
     world_size = xr.world_size()
     print(f"Hello from rank {rank} of {world_size}, pass_rank={pass_rank}")
-    dist.init_process_group("xla", "xla://", rank=rank, world_size=world_size)
+    dist.init_process_group("xla", "xla://")
     dist.barrier()
     print(f"Goodbye from rank {rank} of {world_size}, pass_rank={pass_rank}")
     dist.destroy_process_group()
