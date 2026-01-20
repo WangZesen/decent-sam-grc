f6cc9a8ca2a9e17f47166a55174de982f10f97c0
Zesen <zesen@kth.se>
2026-01-20 15:25:55 +0100

    fix

--- image-cifar/test.py
+++ image-cifar/test.py
@@ -1,19 +1,21 @@
-import os
-import torch
-import torch.distributed as dist
 import torch_xla
-import torch_xla.runtime as xr
 
-
-def run(pass_rank: int):
-    rank = xr.global_ordinal()
-    world_size = xr.world_size()
-    print(f"Hello from rank {rank} of {world_size}, pass_rank={pass_rank}")
-    dist.init_process_group("xla", "xla://")
-    dist.barrier()
-    print(f"Goodbye from rank {rank} of {world_size}, pass_rank={pass_rank}")
-    dist.destroy_process_group()
-
+def test(rank):
+    print(f"Hello World {rank}")
 
 if __name__ == "__main__":
-    torch_xla.launch(run)
+    torch_xla.launch(test)
+
+
+# def run(pass_rank: int):
+#     rank = xr.global_ordinal()
+#     world_size = xr.world_size()
+#     print(f"Hello from rank {rank} of {world_size}, pass_rank={pass_rank}", flush=True)
+#     dist.init_process_group("xla", "xla://")
+#     dist.barrier()
+#     print(f"Goodbye from rank {rank} of {world_size}, pass_rank={pass_rank}", flush=True)
+#     dist.destroy_process_group()
+
+
+# if __name__ == "__main__":
+#     torch_xla.launch(run)
